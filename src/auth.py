9c43efcfa3931821b92f736bb6b7c025440409ff
josh <jgraydus@gmail.com>
2023-03-16 13:19:43 -0600

    add some comments for later me

--- src/auth.py
+++ src/auth.py
@@ -6,10 +6,16 @@ from . import db
 
 auth = Blueprint('auth', __name__)
 
+#
+# show the login page
+#
 @auth.route('/login')
 def login():
     return render_template('login.html')
 
+#
+# this route is called by the login form to authenticate the user
+#
 @auth.route('/login', methods=['POST'])
 def login_post():
     email = request.form.get('email')
@@ -25,30 +31,39 @@ def login_post():
     login_user(user, remember=remember)
     return redirect(url_for('main.decks'))
 
+#
+# log out the current user and redirect to index
+#
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
 
+#
+# show the new user signup form
+#
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
 
+#
+# this route is called by the new user signup form
+#
 @auth.route('/signup', methods=['POST'])
 def signup_post():
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
 
+    # ensure that there isn't already a user with the given email address
     user = db.session.execute(db.select(User).filter_by(email=email)).scalar()
-
     if user:
         flash('email address is not available')
         return redirect(url_for('auth.signup'))
 
+    # create a new user
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-
     db.session.add(new_user)
     db.session.commit()
 
--- src/main.py
+++ src/main.py
@@ -8,23 +8,34 @@ from .models import Deck
 
 main = Blueprint('main', __name__)
 
+#
+# redirect to the decks list
+#
 @main.route('/')
 @login_required
 def index():
     return redirect(url_for('main.decks'))
 
+#
+# show the decks list page
+#
 @main.route('/decks')
 @login_required
 def decks():
     decks = db.session.execute(db.select(Deck).filter_by(owner=current_user.id).order_by(Deck.name)).scalars().all()
     return render_template('decks.html', decks=decks)
 
+#
+# show the current due card for a deck
+# 
+# only the front of the card is shown unless the request includes a 'back' key in its query string
+#
 @main.route('/decks/<int:deck_id>')
 @login_required
 def deck(deck_id):
+    # ensure the current user owns this deck
     owner = current_user.id
     deck = db.session.execute(db.select(Deck).filter_by(id=deck_id,owner=owner)).scalar()
-
     if not deck:
         abort(404)
 
@@ -40,27 +51,33 @@ def deck(deck_id):
 
     return render_template('deck.html', deck=deck, card=card, side=side)
 
+#
+# this route handles processing the "remembered" and "forgot" buttons for a card
+#
 @main.route('/decks/<int:deck_id>/cards/<int:card_id>', methods=['POST'])
 @login_required
 def card_post(deck_id, card_id):
+    # ensure the deck is owned by the current user
     owner = current_user.id
     deck = db.session.execute(db.select(Deck).filter_by(id=deck_id,owner=owner)).scalar()
-
     if not deck:
         abort(404)
 
+    # find the card to update
     card = db.session.execute(db.select(Card).filter_by(id=card_id, deck_id=deck_id)).scalar()
-
     if not card:
         abort(404)
 
     command = request.form['command']
     now = datetime.now(timezone.utc)
 
+    # if the user remembered the card, then increase the time interval until it is due again
     if command == 'remembered':
         due = card.due
         interval = card.interval
         card.interval = interval + 1
+        # the amount of time to add depends on how many consecutive times the user
+        # remembered the card. TODO configure these values better?
         match interval:
             case 0:
                 card.due = due + timedelta(hours=2)
@@ -83,6 +100,7 @@ def card_post(deck_id, card_id):
             case _:
                 card.due = due + timedelta(hours=1024)
 
+    # if the user forgot the card, then make it due again very soon
     if command == 'forgot':
         card.due = now + timedelta(minutes=1)
         card.interval = 0
@@ -92,50 +110,66 @@ def card_post(deck_id, card_id):
 
     return redirect(url_for('main.deck', deck_id=deck_id))
 
+#
+# shows the new-card page
+#
 @main.route('/decks/<int:deck_id>/new-card')
 @login_required
 def new_card(deck_id):
     owner = current_user.id
     deck = db.session.execute(db.select(Deck).filter_by(id=deck_id,owner=owner)).scalar()
-
     if not deck:
         abort(404)
 
     return render_template('new-card.html', deck=deck)
 
+#
+# creates a new card via the form on the new-card page
+#
 @main.route('/decks/<int:deck_id>/new-card', methods=['POST'])
 @login_required
 def new_card_post(deck_id):
+    # ensure that the current user owns this deck
+    owner = current_user.id
+    deck = db.session.execute(db.select(Deck).filter_by(id=deck_id,owner=owner)).scalar()
+    if not deck:
+        abort(404)
+
+    # create the new card
     front = request.form['front']
     back = request.form['back']
-
     now = datetime.now(timezone.utc)
     card = Card(deck_id=deck_id, front=front, back=back, interval=0, due=now)
-
     db.session.add(card)
     db.session.commit()
 
     return redirect(url_for('main.deck', deck_id=deck_id))
 
+#
+# shows the new-deck page
+#
 @main.route('/new-deck')
 @login_required
 def new_deck():
     return render_template('new-deck.html')
 
+#
+# craetes a new deck via the form on the new deck page
+#
 @main.route('/new-deck', methods=['POST'])
 @login_required
 def new_deck_post():
     name = request.form['name']
     owner = current_user.id
 
+    # ensure that the user doesn't already have a deck with the requested name
     deck = db.session.execute(db.select(Deck).filter_by(owner=owner,name=name)).scalar()
-
     if deck:
         flash('a deck with that name exists')
         return redirect(url_for('main.new_deck'))
 
+    # create the new deck
     new_deck = Deck(owner=owner, name=name)
-
     db.session.add(new_deck)
     db.session.commit()
 
